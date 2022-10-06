362daef71e78ca114485d748036d35dfc8ccbedc
Geerson Pizarro <geerson.pizarro@unmsm.edu.pe>
2022-10-06 13:26:28 -0500

    add new changes

--- src/functions/card/controller/card.controller.ts
+++ src/functions/card/controller/card.controller.ts
@@ -73,6 +73,7 @@ export class CardController {
         },
         200
       );
+      dummyCache[event.headers['x-idempotence-key']] = result;
     } catch (error) {
       return formatJSONResponse({ message: 'El token expiro', error: error.message }, 400);
     }
